86b4780ed6e7fa80309bc76f13cf270e0e7f45ec
Chris Pollard <cspollard@gmail.com>
2010-04-04 10:47:24 -0400

    dumps neuron map.

--- nnet.c
+++ nnet.c
@@ -1,32 +1,32 @@
 #include "nnet.h"
 
 
-neuron **initialize(int nneurons) {
-    neuron **pneurons = malloc((nneurons+1)*sizeof(*pneurons));
-    for (int i = 0; i < nneurons; i++) {
-        pneurons[i]->v = ((rand() > RAND_MAX)? 1: -1) *
+neuron **initialize(int n) {
+    neuron **neurons = malloc((n+1)*sizeof(*neurons));
+    for (int i = 0; i < n; i++) {
+        neurons[i] = malloc(sizeof(**neurons));
+        neurons[i]->v = ((rand() > RAND_MAX/2)? 1: -1) *
                     (float) rand() / RAND_MAX;
+        neurons[i]->i = i;
     }
-
-    pneurons[nneurons] = NULL;
-
-    return pneurons;
+    neurons[n] = NULL;
+    return neurons;
 }
 
 
-int connect(neuron *n[], int nmax, int cmax) {
-    int nconnect;
+int connect(neuron *n[], int nmax, int cmax, int cmin) {
     for (int i = 0; n[i]; i++) {
-        n[i]->nc = rand() % cmax;
-        n[i]->c = malloc((nconnect+1)*sizeof(*(n[i]->c)));
-        n[i]->w = malloc((nconnect)*sizeof(*(n[i]->w)));
+        n[i]->nc = (cmax>cmin)? (rand() % (cmax-cmin)) + cmin: cmax;
+        n[i]->c = malloc((n[i]->nc+1)*sizeof(*(n[i]->c)));
+        n[i]->w = malloc((n[i]->nc)*sizeof(*(n[i]->w)));
         for (int j = 0; j < n[i]->nc; j++) {
             n[i]->c[j] = n[rand() % nmax];
-            n[i]->w[j] = ((rand() > RAND_MAX)? 1: -1) *
+            n[i]->w[j] = ((rand() > RAND_MAX/2)? 1: -1) *
                     (float) rand() / RAND_MAX;
         }
         n[i]->c[n[i]->nc] = NULL;
     }
+
     return 0;
 }
 
@@ -42,17 +42,43 @@ int update(neuron **n) {
 }
 
 
-int main(int argc, char *argv[]) {
-
-    neuron **neurons = initialize(10);
-    connect(neurons, 10, 5);
-    update(neurons);
-    update(neurons);
-    update(neurons);
-
-    for (int i = 0; i < 10; i++) {
-        printf("%f\n", neurons[i]->v);
+int dumpconnections(neuron **n) {
+    for (int i = 0; n[i]; i++) {
+        for (int j = 0; n[i]->c[j]; j++) {
+            printf("\t%3d ---(%6.3f)---> %3d\n",
+                    i, n[i]->w[j], n[i]->c[j]->i);
+        }
+        printf("\n");
     }
-   
+    fflush(stdout);
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
+    int nmax = (argc>1)? atoi(argv[1]): 100;
+    int cmax = (argc>2)? atoi(argv[2]): nmax/2;
+    int cmin = (argc>3)? atoi(argv[3]): 0;
+    int niter = (argc>4)? atoi(argv[4]): 100;
+    int njter = (argc>5)? atoi(argv[5]): 100;
+
+    neuron **neurons = initialize(nmax);
+    connect(neurons, nmax, cmax, cmin);
+    printf("madeit!\n");
+    fflush(stdout);
+    dumpconnections(neurons);
+    // printf("%.3f\n", neurons[nmax-1]->v);
+
+    for (int i = 0; i < niter; i++) {
+        for (int j = 0; j < njter; j++) {
+            neurons[0]->v = 1;
+            update(neurons);
+        }
+        //printf("neuron %d: %.3f\n", nmax-1, neurons[nmax-1]->v);
+    }
+
     return 0;
 }
--- nnet.h
+++ nnet.h
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 typedef struct neuron {
     struct neuron **c;
     int nc;
     float *w;
     float v;
+    int i;  // index
 } neuron;
